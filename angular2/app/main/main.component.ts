e38a4b338e5e3195ea35ab87af3f5dcc955337c2
Tamas Foldi <fo.tamas@gmail.com>
2016-01-31 17:06:22 +0100

    angular2

---
+++ angular2/app/main/main.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "angular2/core";
+import {HeaderComponent} from "../header/header.component";
+
+@Component({
+  selector: "tpw-main",
+  templateUrl: "app/main/main.html",
+  directives: [HeaderComponent]
+})
+export class MainComponent {
+
+
+}
