0f7ee433f03d50693de8ebd0d743efa5c40d2a9f
Tamas Foldi <fo.tamas@gmail.com>
2016-02-01 21:42:08 +0100

    [add] menu

---
+++ DTO/Lesson.ts
@@ -0,0 +1,5 @@
+export class Lesson {
+  public id: number;
+  public name: string;
+  public text: string;
+}
---
+++ app/menu/menu.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "angular2/core";
+
+@Component({
+  selector: "tpw-menu",
+  templateUrl: "app/menu/menu.html"
+})
+export class MenuComponent {
+  
+
+}
