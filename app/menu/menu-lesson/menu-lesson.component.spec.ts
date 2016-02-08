01ce03d4bf4ba129c51ae842608e29905d456cb1
Tamas Foldi <fo.tamas@gmail.com>
2016-02-08 18:48:02 +0100

    MenuLessonService refactor

--- app/menu/menu-lesson/menu-lesson.component.spec.ts
+++ app/menu/menu-lesson/menu-lesson.component.spec.ts
@@ -1,25 +1,33 @@
 import { describe, it, expect, inject,
 injectAsync, beforeEach, beforeEachProviders, TestComponentBuilder } from "angular2/testing";
+import {provide} from "angular2/core";
 import {MenuLessonComponent} from "./menu-lesson.component";
 import {MenuLessonService} from "./menu-lesson.service";
 
 describe("Typewriting menu lesson", () => {
   let tcb: TestComponentBuilder;
+  let menuLessonService: any;
 
   beforeEachProviders(() => [
     MenuLessonService
   ]);
 
-  beforeEach(inject([TestComponentBuilder, MenuLessonService], (tcBuilder, menuLessonService) => {
+  beforeEach(inject([TestComponentBuilder], (tcBuilder) => {
+    menuLessonService = new MenuLessonService();
     tcb = tcBuilder;
     spyOn(menuLessonService, "query").and.returnValue([{ id: 1, name: "lesson 1" }]);
+
+    tcb.overrideProviders(MenuLessonComponent, [
+      provide(MenuLessonService, { useValue: menuLessonService })
+    ])
+    .createAsync(MenuLessonComponent);
+
   }));
 
   it("should have one menuLessonElem with name 'lesson 1'", injectAsync([], () => {
     return tcb.createAsync(MenuLessonComponent)
       .then(fixture => {
         let cmp = fixture.componentInstance;
-
         expect(cmp.lessonMenuElems.length).toEqual(1);
         expect(cmp.lessonMenuElems[0].name).toBe("lesson 1");
       });
--- app/menu/menu-lesson/menu-lesson.component.ts
+++ app/menu/menu-lesson/menu-lesson.component.ts
@@ -1,16 +1,24 @@
-import {Component} from "angular2/core";
+import {Component, Inject, OnInit} from "angular2/core";
 import {MenuLessonService} from "./menu-lesson.service";
 import {MenuLesson} from "../../../DTO/MenuLesson";
 
 @Component({
+  providers: [MenuLessonService],
   selector: "tpw-menu-lesson",
   templateUrl: "app/menu/menu-lesson/menu-lesson.html"
 })
 export class MenuLessonComponent {
   lessonMenuElems: Array<MenuLesson>;
 
-  constructor(menuLessonService: MenuLessonService) {
-    this.lessonMenuElems = menuLessonService.query();
+  private menuLessonService: MenuLessonService;
+  constructor( @Inject(MenuLessonService) menuLessonService: MenuLessonService) {
+    this.menuLessonService = menuLessonService;
+
+    this.getMenuItems();
+  }
+
+  getMenuItems() {
+    this.lessonMenuElems = this.menuLessonService.query();
   }
 
 }
