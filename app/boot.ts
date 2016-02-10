3f73c55654e37990505552e357333da7ca08dd50
Tamas Foldi <fo.tamas@gmail.com>
2016-02-10 19:07:40 +0100

    routing basics

--- app/boot.ts
+++ app/boot.ts
@@ -1,3 +1,6 @@
 import {bootstrap}    from "angular2/platform/browser";
 import {MainComponent} from "./main/main.component";
-bootstrap(MainComponent, []);
+import {Component, provide} from "angular2/core";
+import {ROUTER_PROVIDERS, APP_BASE_HREF} from "angular2/router";
+
+bootstrap(MainComponent, [ROUTER_PROVIDERS]);
--- app/main/main.component.ts
+++ app/main/main.component.ts
@@ -1,14 +1,20 @@
 import {Component, ViewChild} from "angular2/core";
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from "angular2/router";
 import {HeaderComponent} from "../header/header.component";
 import {MenuComponent} from "../menu/menu.component";
 import {LoginFormComponent} from "../login/login-form.component";
 import {RegisterFormComponent} from "../register/register-form.component";
 
 @Component({
-  directives: [HeaderComponent, MenuComponent, LoginFormComponent, RegisterFormComponent],
+  directives: [ROUTER_DIRECTIVES, HeaderComponent, MenuComponent],
+  providers: [ROUTER_PROVIDERS],
   selector: "tpw-main",
   templateUrl: "app/main/main.html"
 })
+@RouteConfig([
+  { path: "/login", name: "Login", component: LoginFormComponent, useAsDefault: true },
+  { path: "/register", name: "Register", component: RegisterFormComponent }
+])
 export class MainComponent {
   @ViewChild(HeaderComponent)
   headerComponent: HeaderComponent;
