908f469c9c5b7709f46e123468633786adf936b5
Tamas Foldi <fo.tamas@gmail.com>
2016-02-25 19:39:34 +0100

    typing error fix

--- app/boot.ts
+++ app/boot.ts
@@ -1,3 +1,5 @@
+/// <reference path="../node_modules/angular2/typings/browser.d.ts" /> 
+
 import {bootstrap}    from "angular2/platform/browser";
 import {MainComponent} from "./main/main.component";
 import {Component, provide} from "angular2/core";
