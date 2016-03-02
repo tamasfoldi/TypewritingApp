167e2814c60332a762790049fc9be7b7752836e3
Tamas Foldi <fo.tamas@gmail.com>
2016-03-02 17:38:58 +0100

    init fixes

--- app/app.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../node_modules/angular2/typings/browser.d.ts" /> 
-
-import {bootstrap}    from "angular2/platform/browser";
-import {MainComponent} from "./main/main.component";
-import {Component, provide} from "angular2/core";
-import {ROUTER_PROVIDERS, APP_BASE_HREF} from "angular2/router";
-
-bootstrap(MainComponent, [ROUTER_PROVIDERS]);
