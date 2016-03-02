3d792796b4a3474aa4227ff9d8bed9f43fd6f95c
Tamas Foldi <fo.tamas@gmail.com>
2016-03-02 17:39:20 +0100

    add app component

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from "angular2/core";
+
+@Component({
+  selector: "tpw-game",
+  templateUrl: "app/app.component.html"
+})
+
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+}
