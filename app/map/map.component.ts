cb11258b40e03ce107254af5c5754a05d303bd28
Tamas Foldi <fo.tamas@gmail.com>
2016-03-02 17:39:29 +0100

    add map component

---
+++ app/map/map.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from "angular2/core";
+
+@Component({
+  selector: "tpw-map",
+  templateUrl: "app/map/map.component.html"
+})
+
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+}
