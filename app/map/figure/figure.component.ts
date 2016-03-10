c0fc1ca45478b5bb72553f2807abc9834b56c3d6
Tamas Foldi <fo.tamas@gmail.com>
2016-03-10 09:54:44 +0100

    figure added

---
+++ app/map/figure/figure.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from "angular2/core";
+
+@Component({
+  selector: "twp-figure",
+  templateUrl: "app/map/figure/figure.component.html"
+})
+
+export class FigureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  move() { }
+}
