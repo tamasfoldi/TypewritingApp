4a461d26bc081e28cdb168e928ec9260b3f7311f
tamasfoldi <bucicimaci@gmail.com>
2016-03-28 13:10:35 +0200

    add statistic datas

--- app/typewriter/typewriter.component.ts
+++ app/typewriter/typewriter.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit, Input, ElementRef, ViewChild, AfterViewInit } from "
 import { LessonService, Lesson } from "../lesson/lesson.service";
 import { Pipe, PipeTransform } from "angular2/core";
 
-@Pipe({name: "lessonTextCut"})
+@Pipe({ name: "lessonTextCut" })
 export class LessonTextCutPipe implements PipeTransform {
-  transform(baseText: string, [textToBeCut]) : string {
+  transform(baseText: string, [textToBeCut]): string {
     return baseText.substr(textToBeCut.length, baseText.length);
   }
 }
 
-@Pipe({name: "spaceToUnderscore"})
+@Pipe({ name: "spaceToUnderscore" })
 export class SpaceToUnderscorePipe implements PipeTransform {
-  transform(baseText: string) : string {
+  transform(baseText: string): string {
     return baseText.replace(/( )/g, "_");
   }
 }
@@ -31,12 +31,17 @@ export class TypewriterComponent implements OnInit, AfterViewInit {
   lessonId: number;
   lesson: Lesson;
   typedText: string;
+  correctPresses: number;
+  incorrectPresses: number;
+  timer: number;
 
   constructor(private lessonService: LessonService) { }
 
   ngOnInit() {
     this.lesson = this.lessonService.get(this.lessonId);
     this.typedText = "";
+    this.correctPresses = 0;
+    this.incorrectPresses = 0;
   }
 
   ngAfterViewInit() {
@@ -44,11 +49,36 @@ export class TypewriterComponent implements OnInit, AfterViewInit {
   }
 
   keypress($event: KeyboardEvent) {
-    let char = String.fromCharCode($event.which);
-    let tempTyped = this.typedText + char;
-    if (tempTyped === this.lesson.text.substr(0, tempTyped.length)) {
-      this.typedText = tempTyped;
+    let char = String.fromCharCode($event.which); 
+    if (!this.hasReachedTheEnd()) {
+      if (this.wasItCorrectChar(char)) {
+        this.typedText = this.typedText + char;
+        this.correctPresses++;
+        if (this.wasTheFirstPress(char)) {
+          this.timer = Date.now();
+        }
+      } else {
+        this.incorrectPresses++;
+      }
     }
+    if (this.hasReachedTheEnd()) {
+      this.focus.nativeElement.blur();
+      this.timer = Date.now() - this.timer;
+      // TODO generate statistics
+      console.log(this.correctPresses, this.incorrectPresses, this.timer);
+    }
+  }
+
+  wasItCorrectChar(c: string): boolean {
+    return this.typedText + c === this.lesson.text.substr(0, this.typedText.length + 1);
+  }
+
+  hasReachedTheEnd(): boolean {
+    return this.typedText === this.lesson.text;
+  }
+
+  wasTheFirstPress(c: string): boolean {
+    return this.typedText.length === 1;
   }
 
 }
