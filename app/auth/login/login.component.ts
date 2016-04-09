0c481f8e406e76e9489a85974e2a98fc92c2337f
Tamas Foldi <fo.tamas@gmail.com>
2016-04-09 16:09:34 +0200

    login added

---
+++ app/auth/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "angular2/core";
+import {FormBuilder, ControlGroup, Control, Validators} from "angular2/common";
+@Component({
+  selector: "tpw-login",
+  templateUrl: "app/auth/login/login.component.html"
+})
+export class LoginComponent implements OnInit {
+
+  username: Control;
+  password: Control;
+  loginForm: ControlGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.username = this._formBuilder.control("", Validators.required);
+    this.password = this._formBuilder.control("", Validators.required);
+    this.loginForm = this._formBuilder.group({
+      username: this.username,
+      password: this.password
+    });
+  }
+
+  login(): void {
+    this.reset();
+  }
+
+  reset(): void {
+    this.username.updateValue("");
+    this.password.updateValue("");
+  }
+
+}
