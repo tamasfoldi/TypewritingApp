743e8eea8dfca71f133fe92b650173c4cfe495e1
Tamas Foldi <fo.tamas@gmail.com>
2016-04-19 13:01:04 +0200

    clean server

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,11 @@
 import * as express from "express";
 import * as path from "path";
 import * as jwt from "express-jwt";
-let app = express();
-import { MongoClient  } from "mongodb";
-let dbConn = "mongodb://admin:MAcsek24@ds023560.mlab.com:23560/typewritingapp";
 import * as bodyParser from "body-parser";
 import { lessonRouter } from "./routes/lessons.routes";
 import { userRouter } from "./routes/users.routes";
 
+let app = express();
 let jwtCheck = jwt({
   secret: new Buffer("1Jl2avFWtDPzXN3uJc9WOeXNKUiTNuTHHGWevec-8F6QcyIyZI3-VpdbGXzi-1M9", "base64"),
   audience: "nAG6Yz8t5KQu07YukjV83Wh94hOYiR4T"
