31a5381d038841605220a8f766626216ec33677c
Tamas Foldi <fo.tamas@gmail.com>
2016-04-19 12:03:36 +0200

    errorhandling and stuff

--- server/routes/lessons.routes.ts
+++ server/routes/lessons.routes.ts
@@ -7,23 +7,29 @@ export let lessonRouter = express.Router();
 lessonRouter.get("/", (req, res) => {
   MongoClient.connect(dbConn, (err, db) => {
     if (err) {
-      throw err;
+      res.status(501).send("Internal Server Error")
+      return;
     }
     db.collection("lessons").find({}).toArray().then((result) => {
       res.status(200).send(result);
       db.close();
-    })
+    }, () => {
+      res.status(401).send("Bad request");
+      return;
+    });
   });
 });
 
 lessonRouter.get("/:id", (req, res) => {
   MongoClient.connect(dbConn, (err, db) => {
     if (err) {
-      throw err;
+      res.status(501).send("Internal Server Error")
+      return;
     }
     db.collection("lessons").find({ id: parseInt(req.params.id) }).limit(1).toArray((err, result) => {
       if (err) {
-        throw err;
+        res.status(404).send("Lesson not found");
+        return;
       }
       let lesson = result[0];
       res.status(200).send(lesson);
--- server/routes/users.routes.ts
+++ server/routes/users.routes.ts
@@ -7,11 +7,13 @@ export let userRouter = express.Router();
 userRouter.get("/:email", (req, res) => {
   MongoClient.connect(dbConn, (err, db) => {
     if (err) {
-      throw err;
+      res.status(501).send("Internal Server Error")
+      return;
     }
     db.collection("users").find({ email: req.params.email }).limit(1).toArray((err, result) => {
       if (err) {
-        throw err;
+        res.status(404).send("User not found");
+        return;
       }
       var user = result[0];
       if (!user.lastCompletedLessonId && user.lastCompletedLessonId !== 0) {
@@ -23,14 +25,18 @@ userRouter.get("/:email", (req, res) => {
   });
 });
 
-userRouter.put("/:email", (req, res) => {
+userRouter.put("/last-compoleted-lesson/:email", (req, res) => {
   MongoClient.connect(dbConn, (err, db) => {
     if (err) {
-      throw err;
+      res.status(501).send("Internal Server Error")
+      return;
     }
     db.collection("users").findOneAndUpdate({ email: req.params.email }, { $set: { lastCompletedLessonId: req.body.lastCompletedLessonId } }, (result) => {
       res.status(200).send(result);
       db.close();
+    }, () => {
+      res.status(401).send("Bad request");
+      return;
     });
   });
 });
@@ -38,7 +44,7 @@ userRouter.put("/:email", (req, res) => {
 userRouter.put("/:email/stats/:id", (req, res) => {
   MongoClient.connect(dbConn, (err, db) => {
     if (err) {
-      throw err;
+      res.status(501).send("Internal Server Error")
     }
     let star = Math.floor((Math.random() * 5) + 1);
     let stat = req.body;
@@ -47,17 +53,22 @@ userRouter.put("/:email/stats/:id", (req, res) => {
     stat._star = star;
     db.collection("users").find({ email: req.params.email }).limit(1).toArray((err, result) => {
       if (err) {
-        throw err;
+        res.status(404).send("User not found");
+        return;
       }
       user = result[0];
-      
-      db.collection("statistics").insertOne({ user: user._id, stat });
-      if(!user.lessonStatistics) {
+
+      db.collection("statistics").insertOne({ user: user._id, stat }).then(() => { }, () => {
+        res.status(401).send("Bad request");
+        return;
+      });
+      if (!user.lessonStatistics) {
         user.lessonStatistics = [];
       }
       if (!user.lessonStatistics[idParam] || user.lessonStatistics[idParam]._star < star) {
         user.lessonStatistics[idParam] = stat;
         db.collection("users").updateOne({ email: req.params.email }, user).then(() => {
+
           res.status(200).send(stat);
         });
       } else {
