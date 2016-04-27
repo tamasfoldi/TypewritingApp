bd9ec158ba6c0b69dd5aa85b2b4dcfbcffab2096
Tamas Foldi <fo.tamas@gmail.com>
2016-04-27 22:50:44 +0200

    blinking cursor specs

---
+++ app/util/blinking-cursor/blinking-cursor.component.spec.ts
@@ -0,0 +1,37 @@
+import {BlinkingCursorComponent} from './blinking-cursor.component';
+import {beforeEach, beforeEachProviders, inject, TestComponentBuilder} from "angular2/testing";
+
+describe('Greeter: component', () => {
+  let tcb: TestComponentBuilder;
+
+  //setup
+  beforeEachProviders(() => [
+    TestComponentBuilder,
+    BlinkingCursorComponent
+  ]);
+
+  beforeEach(inject([TestComponentBuilder], _tcb => {
+    tcb = _tcb
+  }));
+
+  //specs
+  it('should render `|`', done => {
+    tcb.createAsync(BlinkingCursorComponent).then(fixture => {
+      let blinkingCurson = fixture.componentInstance;
+      let element = fixture.nativeElement;
+      fixture.detectChanges(); //trigger change detection
+      expect(element.querySelector('span').innerText).toBe('|');
+      done();
+    });
+  });
+
+  it('should has one `blinking-cursor` class', done => {
+    tcb.createAsync(BlinkingCursorComponent).then(fixture => {
+      let blinkingCurson = fixture.componentInstance;
+      let element = fixture.nativeElement;
+      fixture.detectChanges(); //trigger change detection
+      expect(element.querySelectorAll('.blinking-cursor').length).toBe(1);
+      done();
+    });
+  });
+}) 
